11152de553fb920756d4b64e2b8dd03ee0d756d1
@CodeRamuri <coderamuri@gmail.com>
2019-10-14 16:07:25 -0500

    quitamos el ultimo theme de wordpress

---
+++ wp-content/themes/coderamuri/index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+El siguiente comando es es para evitar el problema del
+mensaje
+
+the file will have its original line endings in your working directory
+
+Que siempre sale en windos
+
+git config --global core.autocrlf false
+*/
